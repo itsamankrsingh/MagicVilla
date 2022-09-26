93d5b5cdebb2543dd0d78bd1edf6a5062391a8d9
itsamankrsingh <itsamankrsingh@gmail.com>
2022-09-26 23:49:04 +0530

    added ApplicationDbContext

---
+++ MagicVillaWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MagicVillaWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVillaWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> VillasTable { get; set; }
+    }
+}
--- MagicVillaWebAPI/Models/Dto/VillaDto.cs
+++ MagicVillaWebAPI/Models/Dto/VillaDto.cs
@@ -10,5 +10,10 @@ namespace MagicVillaWebAPI.Models.Dto
         public string Name { get; set; }
         public int Occupancy { get; set; }
         public int Sqft { get; set; }
+        [Required]
+        public double Rate { get; set; }
+        public string Details { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
     }
 }
