2a35a4b4b3e439b620404766ccaa1f6964b0845e
itsamankrsingh <itsamankrsingh@gmail.com>
2022-11-02 11:20:12 +0530

    Delete Villa api consumed in MagicVillaWeb

--- MagicVillaWeb/Controllers/VillaController.cs
+++ MagicVillaWeb/Controllers/VillaController.cs
@@ -70,5 +70,27 @@ namespace MagicVillaWeb.Controllers
             }
             return View(model);
         }
+
+        public async Task<IActionResult> DeleteVilla(int villaId)
+        {
+            var response = await _villaService.GetAsync<APIResponse>(villaId);
+            if (response != null && response.IsSuccess)
+            {
+                VillaDto model = JsonConvert.DeserializeObject<VillaDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteVilla(VillaDto model)
+        {
+            var response = await _villaService.DeleteAsync<APIResponse>(model.Id);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(IndexVilla));
+            }
+            return View(model);
+        }
     }
 }
